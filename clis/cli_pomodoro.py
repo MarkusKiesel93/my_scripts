5009498b43323736ce0d5f3fe938bce7891c088d
MarkusKiesel93 <markus.kiesel@tuta.io>
2021-02-02 20:25:14 +0100

    create CLI for pomodoro

---
+++ clis/cli_pomodoro.py
@@ -0,0 +1,28 @@
+from .base import CliBase
+from scripts.timer import Timer
+
+
+class CliPomodoro(CliBase):
+    def __init__(self):
+        self.parser_description = 'Pomodoro for productive working'
+
+    def execute(self, args):
+        pomodoro = Timer(args.min_pomo, output='POMODORO')
+        short_break = Timer(args.short_break, output='SHORT BREAK')
+        long_break = Timer(args.long_break, output='LONG BREAK')
+
+        # run pomodoro
+        while True:
+            pomodoro.start()
+            short_break.start()
+            pomodoro.start()
+            long_break.start()
+
+            if not args.loop:
+                break
+
+    def create_parser(self, sub_parser):
+        sub_parser.add_argument('min_pomo', type=int, nargs='?', default=25, help='minutes for pomodoro')
+        sub_parser.add_argument('short_break', type=int, nargs='?', default=5, help='minutes for short break')
+        sub_parser.add_argument('long_break', type=int, nargs='?', default=15, help='minutes for long break')
+        sub_parser.add_argument('-lo', '--loop', action='store_true')
