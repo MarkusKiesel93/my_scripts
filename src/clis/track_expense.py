4afcc65120d5edb34184d369b73e10579790bc51
MarkusKiesel93 <markus.kiesel@tuta.io>
2021-02-03 09:41:24 +0100

    cli for tracking expenses in csv file

---
+++ src/clis/track_expense.py
@@ -0,0 +1,26 @@
+from src.base_cli import CliBase
+from src.tracker.finances.expense_tracker import ExpenseTracker
+
+
+class CliTrackExpense(CliBase):
+    def __init__(self):
+        self.parser_description = 'Track my expanses in data file'
+
+    def execute(self, args):
+        expense_tracker = ExpenseTracker()
+        if args.list:
+            expense_tracker.list(args.list)
+        elif args.add:
+            expense_tracker.add()
+        elif args.donate:
+            expense_tracker.donate()
+        else:
+            print('select one option')
+
+    def create_parser(self, sub_parser):
+        sub_parser.add_argument(
+            '-a', '--add', action='store_true', help='add a new expense to the database')
+        sub_parser.add_argument(
+            '-ls', '--list', type=int, nargs='?', const=10, metavar='x', help='list the last x rows')
+        sub_parser.add_argument(
+            '-d', '--donate', action='store_true', help='add a donation to the expense database')
---
+++ src/tracker/finances/expense_tracker.py
@@ -0,0 +1,72 @@
+import pandas as pd
+from src.db_path_loader import DbPathLoader
+from src.cli_inquirer import ask_date, ask_string, ask_float, ask_choices
+
+
+class ExpenseTracker:
+    def __init__(self):
+        self.db_path = DbPathLoader().get('expenses')
+        self.db = pd.read_csv(self.db_path)
+
+    def add(self):
+        finished = False
+        while not finished:
+            self._ask_new_expense()
+            message = 'add another expense ?'
+            choices = ['yes', 'no']
+            answer = ask_choices('finished', choices, message)
+            if answer == 'no':
+                finished = True
+        self._save_db()
+
+    def donate(self):
+        self._ask_new_donation()
+        self._save_db()
+
+    def list(self, num_rows=10):
+        print(self.db.tail(num_rows))
+
+    def _save_db(self):
+        self.list()
+        self.db.to_csv(path_or_buf=self.db_path, index=False)
+        print('successfully added data')
+
+    def _ask_new_expense(self):
+        new_expense = {'date': '', 'item': '', 'place': '', 'amount': 0.0, 'category': '', 'sub_category': ''}
+        new_expense['date'] = ask_date()
+        new_expense['item'] = ask_string('item')
+        new_expense['amount'] = ask_float('amount')
+        new_expense['category'] = self._inquire_user('category', new_expense['item'])
+        new_expense['sub_category'] = self._inquire_user('sub_category', new_expense['item'])
+        new_expense['place'] = self._inquire_user('place', new_expense['item'])
+
+        self.db = self.db.append(new_expense, ignore_index=True)
+
+    def _ask_new_donation(self):
+        new_expense = {'date': '', 'item': '', 'place': '', 'amount': 0.0, 'category': '', 'sub_category': ''}
+        new_expense['date'] = ask_date()
+        new_expense['item'] = 'donation'
+        new_expense['amount'] = ask_float('amount')
+
+        self.db = self.db.append(new_expense, ignore_index=True)
+
+    def _inquire_user(self, column, item):
+        choices = self._get_choices(column, item)
+        message = column.replace('_', ' ')
+        answer = ask_choices(column, choices, message)
+        if answer == 'new':
+            new = input(f'new {column.replace("_", " ")}:\n')
+            return new
+        elif answer == 'blank':
+            return ''
+        else:
+            return answer
+
+    def _get_choices(self, column, item):
+        choices = self.db.query(f'item == "{item}"')
+        choices = choices[column]
+        choices = choices.drop_duplicates()
+        choices = choices.dropna()
+        choices = choices.tolist()
+        choices += ['blank', 'new']
+        return choices
