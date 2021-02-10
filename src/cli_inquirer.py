b2df868c4f57399fbf60601161ec2cfe2954ebb8
MarkusKiesel93 <markus.kiesel@tuta.io>
2021-02-10 10:08:46 +0100

    rework inquirer

--- src/cli_inquirer.py
+++ src/cli_inquirer.py
@@ -3,26 +3,25 @@ import datetime
 import sys
 from curtsies import Input
 
-# TODO: add output messages as function arguments
-
 
 def ask_date():
+    """
+    inquires user four date
+    navigate with ARROW UP, ARROW DOWN and RETURN KEY
+    returns datetime date object
+    """
     today = datetime.date.today()
     days_delta = 0
     print('date:')
-    # output in same line
     date = today
     sys.stdout.write(f'\r{date}')
-    # use curtsies for Key input
-    # decrese date with KEY_UP, incrise date with KEY_DOWN
-    # select with return
     with Input(keynames='curses') as input_generator:
         for e in input_generator:
-            if e == 'KEY_UP':
+            if e == 'KEY_UP':  # ARROW UP
                 days_delta += 1
-            if e == 'KEY_DOWN':
+            if e == 'KEY_DOWN':  # ARROW DOWN
                 days_delta -= 1
-            if e == '\n':
+            if e == '\n':  # RETURN KEY
                 break
             date = today - datetime.timedelta(days=days_delta)
             sys.stdout.write(f'\r{date}')
@@ -30,28 +29,55 @@ def ask_date():
     return date
 
 
-# TODO: only use input once
-def ask_string(prompt_name):
-    item = input(f'{prompt_name}:\n')
-    while len(item) <= 1:
-        print(f'{prompt_name} needs to be given!')
-        item = input(f'{prompt_name}:\n')
-    return item
+def ask_string(prompt_message):
+    """
+    inquires user for some string
+    prompt_message: message prompted to the user
+    returns user Input as String
+    """
+    while True:
+        item = input(f'{prompt_message}:\n')
+        if _satisfying_answer(item):
+            break
+    return str(item)
 
 
-def ask_float(prompt_name):
+def ask_float(prompt_message):
+    """
+    inquires user for some float value
+    prompt_message: message prompted to the user
+    returns user Input as Float
+    """
     amount = None
     while type(amount) is not float:
         try:
-            amount = input(f'{prompt_name}:\n')
+            amount = input(f'{prompt_message}:\n')
             amount = float(amount)
         except ValueError:
-            print(f'{prompt_name} has to be given as float')
+            print(f'{prompt_message} has to be given as float')
     return amount
 
 
-# TODO: not tested and not used
-def ask_choices(name, choices, message):
+def ask_choices(name, choices, message, ask_new=False):
+    """
+    inquires user to selet one of the given choices
+    if ask_new is True the user can select adding a new option
+    returns user choice as String
+    """
+    if len(choices) < 1:
+        raise Exception('at least one choice has to be given')
+    if ask_new:
+        choices += ['new']
     questions = [inquirer.List(name, message=message, choices=choices)]
-    answers = inquirer.prompt(questions)
-    return answers[name]
+    answer = inquirer.prompt(questions)[name]
+    if ask_new and answer == 'new':
+        answer = ask_string(name)
+    return answer
+
+
+# check user input
+def _satisfying_answer(answer):
+    if len(answer) < 3:
+        print('needs input of at least 3 characters!')
+        return False
+    return True
